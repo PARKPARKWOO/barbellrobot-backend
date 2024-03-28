b784786f17e0ffb7c7e863eb5cf88ecb59ffd583
PARKPARKWOO <wy9295@naver.com>
2024-03-29 00:03:41 +0900

    chore: lint

--- core/src/main/kotlin/com/example/core/history/dto/HistoryResponseDto.kt
+++ core/src/main/kotlin/com/example/core/history/dto/HistoryResponseDto.kt
@@ -11,50 +11,50 @@ data class HistoryResponseDto(
 )
 
 data class UserHistoryResponseDto
-@QueryProjection
-constructor(
-    val id: UUID,
-    val today: LocalDate,
-    val attendance: Boolean,
-    val dietFoodDtos: List<DietFoodQueryDto>,
-    val dietImageDtos: List<DietImageQueryDto>,
-    val todayImageIds: List<String>?,
-    val todayVideo: List<String>?,
-)
+    @QueryProjection
+    constructor(
+        val id: UUID,
+        val today: LocalDate,
+        val attendance: Boolean,
+        val dietFoodDtos: List<DietFoodQueryDto>,
+        val dietImageDtos: List<DietImageQueryDto>,
+        val todayImageIds: List<String>?,
+        val todayVideo: List<String>?,
+    )
 
 data class UserHistoryQueryDto
-@QueryProjection
-constructor(
-    val id: UUID,
-    val today: LocalDate,
-    val attendance: Boolean,
-    var todayImageIds: List<Long>?,
-    var todayVideoIds: List<Long>?,
-)
+    @QueryProjection
+    constructor(
+        val id: UUID,
+        val today: LocalDate,
+        val attendance: Boolean,
+        var todayImageIds: List<Long>?,
+        var todayVideoIds: List<Long>?,
+    )
 
 data class DietFoodQueryDto
-@QueryProjection
-constructor(
-    val historyId: UUID,
-    val food: String,
-    val type: Diet,
-)
+    @QueryProjection
+    constructor(
+        val historyId: UUID,
+        val food: String,
+        val type: Diet,
+    )
 
 data class DietImageQueryDto
-@QueryProjection
-constructor(
-    val historyId: UUID,
-    val imageUrl: String,
-    val type: Diet,
-)
+    @QueryProjection
+    constructor(
+        val historyId: UUID,
+        val imageUrl: String,
+        val type: Diet,
+    )
 
 data class ExerciseHistoryResponseDto
-@QueryProjection
-constructor(
-    val id: Long,
-    val itemId: Long,
-    val weight: Double,
-    val exerciseSet: Int,
-    val userHistoryId: UUID,
-    val createdAt: LocalDate,
-)
+    @QueryProjection
+    constructor(
+        val id: Long,
+        val itemId: Long,
+        val weight: Double,
+        val exerciseSet: Int,
+        val userHistoryId: UUID,
+        val createdAt: LocalDate,
+    )
