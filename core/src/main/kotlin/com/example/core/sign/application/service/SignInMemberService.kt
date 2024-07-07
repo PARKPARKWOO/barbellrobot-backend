b0da139142e325eae28c6d08bc0df5ef035b7979
PARKPARKWOO <wy9295@naver.com>
2024-07-07 18:50:51 +0900

    draft

--- core/src/main/kotlin/com/example/core/sign/application/service/SignInMemberService.kt
+++ core/src/main/kotlin/com/example/core/sign/application/service/SignInMemberService.kt
@@ -14,6 +14,7 @@ import com.example.domain.user.SocialProvider
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.util.*
 
 @Service
 class SignInMemberService(
