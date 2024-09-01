4b632de7c4aeb4ecf888de7cfbf8ff75a062a25d
PARKPARKWOO <wy9295@naver.com>
2024-09-01 16:30:20 +0900

    chore: Add scheduling annotaion

--- api/src/main/kotlin/com/example/in/scheduler/SseHeartbeatScheduler.kt
+++ api/src/main/kotlin/com/example/in/scheduler/SseHeartbeatScheduler.kt
@@ -1,7 +1,6 @@
 package com.example.`in`.scheduler
 
 import com.example.application.common.log.logger
-import com.example.infrastructure.adapter.notification.SseConnectionRegistry
 import com.example.infrastructure.adapter.notification.SseConnectionRegistry.SSE_CONNECTED_USER
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
@@ -18,7 +17,6 @@ class SseHeartbeatScheduler {
 
     @Scheduled(fixedDelay = HEARTBEAT_INTERVAL)
     fun sendHeartbeat() {
-        log.info(Thread.currentThread().name)
         SSE_CONNECTED_USER.forEach { (userId, sseEmitter) ->
             runCatching {
                 sseEmitter.send(SseEmitter.event().name(HEARTBEAT_EVENT_NAME))
